0c1d1f906e5a4fa72993af2eda69ce50482899f3
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-10-03 19:09:50 +0300

    +2 algorithma

---
+++ 21 Decimical to Factorial and Back.py
@@ -0,0 +1,22 @@
+def dec_2_fact_string(nb):
+    factorial_string, i = '', 1
+    while nb != 0:
+        if nb % i > 9:
+            factorial_string += chr(65 + (nb % i) - 10)
+        else:
+            factorial_string += str(nb % i)
+        nb = nb // i
+        i += 1
+    return factorial_string[::-1]
+
+def fact_string_2_dec(string):
+    nb = 0
+    for i in range(len(string)):
+        if str(string[i]) not in '0123456789':
+            nb = nb * (len(string) - i) + int((ord(string[i]))) - 55
+        else:
+            nb = nb * (len(string) - i) + int(string[i])
+    return nb
+
+print(dec_2_fact_string(36288000))
+print(fact_string_2_dec("010000000000"))
